3e3de94d1de70ff0619c557d016faa86dcc6d7b8
Lucas Lauret <llauret.25@gmail.com>
2025-09-25 08:42:23 +0000

    Modules

---
+++ IHM/src/main/java/fr/ufrst/m1info/gl/groupe7/App.java
@@ -0,0 +1,13 @@
+package fr.ufrst.m1info.gl.groupe7;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Memoire/src/test/java/fr/ufrst/m1info/gl/groupe7/AppTest.java
@@ -0,0 +1,20 @@
+package fr.ufrst.m1info.gl.groupe7;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
