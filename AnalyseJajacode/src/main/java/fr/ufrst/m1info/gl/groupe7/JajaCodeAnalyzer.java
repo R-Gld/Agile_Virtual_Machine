b35a2c3aa7c39f9e08c59d2b1f3619235ecaa680
Lucas Lauret <llauret.25@gmail.com>
2025-10-05 20:01:12 +0200

    feat: update JajaCodeAnalyzer to accept file name as argument and read from classpath + ressources directory

--- AnalyseJajacode/src/main/java/fr/ufrst/m1info/gl/groupe7/JajaCodeAnalyzer.java
+++ AnalyseJajacode/src/main/java/fr/ufrst/m1info/gl/groupe7/JajaCodeAnalyzer.java
@@ -4,12 +4,19 @@ import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class JajaCodeAnalyzer {
     public static void main(String[] args) throws IOException {
-        String filename = "AnalyseJajacode/src/main/java/fr/ufrst/m1info/gl/groupe7/jajacode.txt";
-        CharStream input = CharStreams.fromFileName(filename);
-        JajaCodeLexer lexer = new JajaCodeLexer(input);
+        if (args.length == 0) {
+            System.err.println("Veuillez fournir le nom du fichier en argument. ex : jajacode.txt");
+            return;
+        }
+        InputStream input = JajaCodeAnalyzer.class.getClassLoader().getResourceAsStream(args[0]);
+        assert input != null;
+
+        CharStream charStream = CharStreams.fromStream(input);
+        JajaCodeLexer lexer = new JajaCodeLexer(charStream);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         JajaCodeParser parser = new JajaCodeParser(tokens);
 
