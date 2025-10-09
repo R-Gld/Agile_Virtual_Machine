1ec550e686c45cb90370632ed84e6a0c3aca717b
Félix Riat <felix.riat@edu.univ-fcomte.fr>
2025-10-09 22:30:31 +0200

    Création interface graphique, decouverte de l'objet codeArea de richtextfx

--- IHM/src/main/java/fr/ufrst/m1info/gl/groupe7/App.java
+++ IHM/src/main/java/fr/ufrst/m1info/gl/groupe7/App.java
@@ -2,9 +2,9 @@ package fr.ufrst.m1info.gl.groupe7;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
 
 
 /**
@@ -14,11 +14,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        String codeSample = "class C {\n\tint x = 0;\n\n\tmain {\n\t\tx = 12;\n\t}\n}";
+        CodeArea codeArea = new CodeArea(codeSample);
+        var scene = new Scene(codeArea, 640, 480);
+        codeArea.setWrapText(true);
+        codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
         stage.setScene(scene);
         stage.show();
     }
--- IHM/src/main/java/module-info.java
+++ IHM/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module fr.ufrst.m1info.gl.groupe7 {
     requires javafx.controls;
+    requires org.fxmisc.richtext;
     exports fr.ufrst.m1info.gl.groupe7;
 }
