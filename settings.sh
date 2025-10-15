31d30b406973b7af6fd17b86adcb23ae9d2811e5
Romain Galland <romain.galland@infomaniak.com>
2025-10-15 20:55:31 +0200

    ci(gitlab): update Maven settings file path in CI configuration
    Changed the Maven settings file path from the default location to a relative path in the CI configuration, ensuring consistency across different environments.

--- settings.sh
+++ settings.sh
@@ -5,7 +5,7 @@ if [[ -z $HOME ]]; then
   exit 1
 fi
 
-cat <<EOF > settings.xml
+cat <<EOF > .settings.xml
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">
   <servers>
     <server>
@@ -68,9 +68,6 @@ cat <<EOF > settings.xml
   </profiles>
 </settings>
 EOF
-
-# Move settings.xml to the .m2 directory of the runner's container
-mv settings.xml "$HOME"/.m2/settings.xml
 
 # Create the settings-security.xml file from a template
 cat <<EOF > settings-security.xml
