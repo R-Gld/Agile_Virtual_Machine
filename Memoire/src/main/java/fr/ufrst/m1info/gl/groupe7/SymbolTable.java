aa92257909caf0e7bfcde8a32b6c37e0a2070b34
Romain Galland <romain.galland@infomaniak.com>
2025-11-02 14:03:37 +0100

    fix(DeclsNode, SymbolTable): handle null cases in string representation and type retrieval
    Updated the string representation in DeclsNode to handle null declarations more gracefully.
    In SymbolTable, added a null check for the node before accessing its type to prevent potential
    NullPointerExceptions.

--- Memoire/src/main/java/fr/ufrst/m1info/gl/groupe7/SymbolTable.java
+++ Memoire/src/main/java/fr/ufrst/m1info/gl/groupe7/SymbolTable.java
@@ -224,7 +224,9 @@ public class SymbolTable {
     /** return type of node */
     public String type(String name) {
         if (contains(name)) {
-            return findNode(name).quad.type;
+            Node node = findNode(name);
+            if (node == null) return null;
+            return node.quad.type;
         } else {
             return null;
         }
