71ee2b12c47cb057c267c94ffecc1aa9a62d9c04
Félix Riat <felix.riat@edu.univ-fcomte.fr>
2025-11-02 18:44:13 +0100

    Fix dependencies for mvn package

--- IHM/src/main/java/module-info.java
+++ IHM/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module IHM {
     requires javafx.controls;
     requires org.fxmisc.richtext;
     requires org.fxmisc.flowless;
-    requires LexerParser;
     requires java.desktop;
     requires compiler;
     exports fr.ufrst.m1info.gl.groupe7.IHM;
--- compiler/src/main/java/module-info.java
+++ compiler/src/main/java/module-info.java
@@ -1,7 +1,5 @@
 module compiler {
     exports fr.ufrst.m1info.gl.groupe7.compiler;
-    requires LexerParser;
-    requires Memoire;
-    requires org.antlr.antlr4.runtime;
+    requires transitive LexerParser;
     requires org.apache.commons.cli;
 }
