b5be7ef4ebedffd1d70b3ed0731454b4b0159133
Léo Maugeri <leomaugeri25@gmail.com>
2025-11-22 21:58:56 +0100

    Add ParseTreeImageExporter utility and update AST node string representations
    - Introduced ParseTreeImageExporter for parsing MiniJaja code and exporting syntax trees as PNG images.
    - Removed redundant toString methods in DeclsNode and VarsNode classes.
    - Added example images for AST and parse tree.

--- LexerParser/src/main/java/fr/ufrst/m1info/gl/groupe7/lexerparser/minijaja/ast/vars/VarsNode.java
+++ LexerParser/src/main/java/fr/ufrst/m1info/gl/groupe7/lexerparser/minijaja/ast/vars/VarsNode.java
@@ -26,17 +26,10 @@ public class VarsNode extends AstNode {
     }
 
     @Override
-    public String toString() {
-        return var.toString() + (vars != null ? ";" + vars : "");
-    }
-    @Override
     public String toStringTree() {
-        StringBuilder sb = new StringBuilder();
-
-        if (vars == null)   return "vnil";
-        else                sb.append("vars (").append(var.toStringTree()).append(",").append(vars.toStringTree()).append(")");
-
-        return sb.toString();
+        if (var == null)
+            return "vnil";
+        return "vars (" + var.toStringTree() + "," + (vars != null ? vars.toStringTree() : "vnil") + ")";
     }
 
 }
