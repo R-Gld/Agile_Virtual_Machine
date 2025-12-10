25f5db98c1ad815e48134b4161218abff9e925c3
Romain Galland <romain.galland@infomaniak.com>
2025-12-10 17:07:49 +0100

    test(TestCodeArea): remove macOS specific test disabling
    Removed the @DisabledOnOs annotation for tests related to auto-completion popups, as they are now stable on macOS.

--- GUI/src/test/java/fr/ufrst/m1info/gl/groupe7/gui/TestCodeArea.java
+++ GUI/src/test/java/fr/ufrst/m1info/gl/groupe7/gui/TestCodeArea.java
@@ -6,8 +6,6 @@ import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnOs;
-import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
@@ -138,7 +136,6 @@ public class TestCodeArea {
         }
     }
 
-    @DisabledOnOs(value = OS.MAC, disabledReason = "Auto-completion popup tests are unstable on macOS")
     @Test
     void testAutoCompletionPopupAppears(FxRobot robot) {
         runOnFxThread(() -> {
@@ -161,7 +158,6 @@ public class TestCodeArea {
         Assertions.assertFalse(robot.lookup(".context-menu").queryAll().isEmpty(), "Auto-completion popup should be visible");
     }
 
-    @DisabledOnOs(value = OS.MAC, disabledReason = "Auto-completion popup tests are unstable on macOS")
     @Test
     void testAutoCompletionToggle(FxRobot robot) {
         runOnFxThread(() -> {
