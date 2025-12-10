fc5dc93ddebec0d73239491e16e4a47e544fdaf6
Romain Galland <romain.galland@infomaniak.com>
2025-12-10 09:34:08 +0100

    refactor(Omega): add documentation for Omega singleton class
    Added a Javadoc comment to the Omega class to describe its purpose as a singleton representing the special Omega value in the memory model, used to denote uninitialized values.

--- Memoire/src/main/java/fr/ufrst/m1info/gl/groupe7/memoire/omega/Omega.java
+++ Memoire/src/main/java/fr/ufrst/m1info/gl/groupe7/memoire/omega/Omega.java
@@ -1,11 +1,13 @@
 package fr.ufrst.m1info.gl.groupe7.memoire.omega;
 
+/**
+ * Omega singleton class representing the special Omega value in the memory model.
+ * Used to denote uninitialized value.
+ */
 public class Omega {
+
     private static final Omega instance = new Omega();
-
-    private Omega() {
-    }
-
+    private Omega() {}
     public static Omega getInstance() {
         return instance;
     }
